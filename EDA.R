5c59b5e2d87016a0da521497447acec3a9681c57
Adam Ficke <aficke@gmail.com>
2020-02-12 17:55:25 -0500

    plot 1, plus refined data import

--- EDA.R
+++ EDA.R
@@ -1,28 +1,47 @@
-#Adam Ficke
-#Exporatory Data Analysis Peer Graded Assignment 
+#Author - Adam Ficke
+#Exporatory Data Analysis Peer Graded Assignment
 
-#read in data table 
-elec.data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE) 
+#read in data table
+elec.data <-
+  read.table(
+    "household_power_consumption.txt",
+    header = TRUE,
+    sep = ";",
+    stringsAsFactors = FALSE
+  )
 
-#convert factors to date/time 
+#convert factors to date/time
 
-elec.data$DateTime <- paste(elec.data$Date,elec.data$Time)
-elec.data$DateTime <- strptime(elec.data$DateTime, format = "%d/%m/%Y %H:%M:%S")
+elec.data$DateTime <- paste(elec.data$Date, elec.data$Time)
+elec.data$DateTime <-
+  strptime(elec.data$DateTime, format = "%d/%m/%Y %H:%M:%S")
 elec.data$Date <- as.Date(elec.data$Date, format = "%d/%m/%Y")
 
+#subset to days in question
+elec.data.small <-
+  subset(elec.data, Date >= "2007-02-01" & Date <= "2007-02-02")
+
 #check format
-str(elec.data)
+str(elec.data.small)
 
-#subset to days in question 
-elec.data.small<- subset(elec.data,Date>="2007-02-01" & Date <= "2007-02-02")
-  
-  
+#convert to numeric
 
+elec.data.small$Global_active_power <-
+  as.numeric(elec.data.small$Global_active_power)
+
+#check for NAs
+test <-
+  elec.data.small[is.na(elec.data.small$Global_active_power),]
 
 #Global Active Power
 
-hist(elec.data$Global_active_power)
+hist(
+  elec.data.small$Global_active_power,
+  col = "red",
+  main = "Global Active Power",
+  xlab = "Global Active Power (kilowatts)",
+  ylab = "Frequency"
+)
 
-
-summary(elec.data)
-
+dev.copy(png, "Plot 1.png", width = 480, height = 480)
+dev.off()
