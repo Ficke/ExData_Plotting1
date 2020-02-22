9bb399dec09954c0c2e2a8346960e404dc57b974
Adam Ficke <aficke@gmail.com>
2020-02-21 22:22:03 -0500

    finished with plot 2

--- plot2.R
+++ plot2.R
@@ -1,6 +1,9 @@
 #Author - Adam Ficke
 #Exporatory Data Analysis Peer Graded Assignment
 
+#load libraries
+library(datasets)
+
 #read in data table
 elec.data <-
   read.table(
@@ -11,39 +14,28 @@ elec.data <-
   )
 
 #convert date & time factors to date/time
-
 elec.data$DateTime <- paste(elec.data$Date, elec.data$Time)
 elec.data$DateTime <-
   strptime(elec.data$DateTime, format = "%d/%m/%Y %H:%M:%S")
-#elec.data$DateTime <- as.POSIXct(elec.data$DateTime)
 elec.data$Date <- as.Date(elec.data$Date, format = "%d/%m/%Y")
 
 #subset to days in question
 elec.data.small <-
   subset(elec.data, Date >= "2007-02-01" & Date <= "2007-02-02")
 
-#convert to numeric
-
+#convert Power to numeric
 elec.data.small$Global_active_power <-
   as.numeric(elec.data.small$Global_active_power)
 
 #Plot2
 
-library(datasets)
-library(ggplot2)
-#elec.data.small$Weekday <- weekdays.Date(elec.data.small$Date)
+plot(
+  elec.data.small$DateTime,
+  elec.data.small$Global_active_power,
+  ylab = "Gobal Average Power (kilowatts)",
+  type = "l",
+  xlab = NA
+)
 
-elec.data.small$weekday<-weekdays.Date(elec.data.small$DateTime)
-
-#qplot(elec.data.small$DateTime,elec.data.small$Global_active_power)
-#ggplot(data = elec.data.small, aes(x=DateTime,y=Global_active_power)) +
-# geom_smooth(method = "lm")
-
-#plot(Global_active_power ~ DateTime, elec.data.small,type="l")
-
-plot(elec.data.small$DateTime,
-     elec.data.small$Global_active_power,ylab = "Gobal Average Power (kilowatts)",type = "l")
-
-
-dev.copy(png, "plot1.png", width = 480, height = 480)
+dev.copy(png, "plot2.png", width = 480, height = 480)
 dev.off()
